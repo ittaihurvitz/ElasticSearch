1456e833577805dbda2c558ae67642aa50b1957b
ittaihu <ittaihu@gmail.com>
2018-08-29 10:55:21 +0300

    Added script to update the mapping

---
+++ updateMappings.py
@@ -0,0 +1,10 @@
+# This code sceeded to update the mapping called "type1" under index "bubble_list
+# it updated the field "bubble_name" to have "fielddata": true
+
+import requests
+
+url     = 'http://localhost:9200/bubbles_list/_mapping/type1'
+payload = '{  "properties": {"bubble_name": {"type": "text", "fielddata": true }}}'
+headers = {'Content-Type': 'application/json'}
+res = requests.put(url, data=payload, headers=headers)
+print res
